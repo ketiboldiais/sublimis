4f5af643b31456ed5b9b9183506a92f273386c61
Ketib Oldiais <numerstack@gmail.com>
2022-06-04 19:56:12 -0500

    Refactor Layout component (theme toggler made as a separate component)

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,14 @@
 import React from "react";
 import styles from "../styles/Layout.module.css";
-import { useState, useEffect } from "react";
-import Nav from "./Nav";
+import { NavBody } from "./Nav";
 
 const Layout = ({ children }) => {
-	const [theme, setTheme] = useState("");
-	useEffect(() => {
-		let localTheme = window.localStorage.getItem("theme");
-		setTheme(localTheme);
-	}, []);
-
-	const switchTheme = () => {
-		const newTheme = theme === "dark" ? "light" : "dark";
-		window.localStorage.setItem("theme", newTheme);
-		setTheme(newTheme);
-	}
 	return (
-		<div className={`${styles.mainContent} ${theme}`}>
-			<Nav clickHandler={() => switchTheme()} theme={theme}/>
-			<div className={styles.container}>
-				<main className={styles.main}>{children}</main>
-			</div>
+		<div className={styles.master}>
+			<nav className={styles.navbar}>
+				<NavBody />
+			</nav>
+			<main className={styles.page}>{children}</main>
 		</div>
 	);
 };
