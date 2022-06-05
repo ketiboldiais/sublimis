b670d3a59bfbe7a4a2f2b644950b159d57a6b870
Ketib Oldiais <numerstack@gmail.com>
2022-06-04 19:54:58 -0500

    Ensure all flowcharts are set to "fit"

--- components/FlowChart.js
+++ components/FlowChart.js
@@ -4,24 +4,23 @@ const Graphviz = dynamic(() => import("graphviz-react"), { ssr: false });
 
 export const FlowChart = ({
 	data,
-	height = 150,
-	width = 150,
+	height = 200,
+	width = 200,
 	type = "digraph",
-	nodeShape = "box",
+	nodeShape = "plain",
 	nodeHeight = 0.2,
 	rankdir = "TB",
-	arrowSize=0.4,
-	fontSize=7,
+	arrowSize = 0.4,
+	fontSize = 7,
 	engine = "dot",
 }) => {
-	// const dot = data;
 	return (
 		<figure className="flowchart">
 			<Graphviz
 				options={{
 					className: "flowchart",
-					width: width,
-					height: height,
+					// width: width,
+					// height: height,
 					zoom: false,
 					useWorker: false,
 					engine: engine,
